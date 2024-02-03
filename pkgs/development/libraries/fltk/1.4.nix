1a33855542831d845c3e7b72bdd8ead7f6ec8c9a
Idris Raja <idris.raja@gmail.com>
2024-02-02 21:00:07 -0600

    Update 1.4.nix

--- pkgs/development/libraries/fltk/1.4.nix
+++ pkgs/development/libraries/fltk/1.4.nix
@@ -1,5 +1,5 @@
 import ./common.nix rec {
   version = "1.4";
   rev = "b21a3910a967170c1a022f22ffa8ecb98c308f37";
-  sha256 = "";
+  sha256 = "sha256-XxXvMkG5MOpe4IfsTBAJrRyqgQ9qTpD7EZXxgIeE5Ww=";
 }
